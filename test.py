21bdb932088a66fc520cf8c42f81d91b4153b98b
Nick Yeow <ytknick@gmail.com>
2024-11-12 23:57:16 +0800

    Text test file

--- test.py
+++ test.py
@@ -1,32 +1,23 @@
 import unittest
 
-from dictgen import gen
-
-# TODO add stress to pronunciation
+from dictgen import Stroke, gen
 
 class TestDictgen(unittest.TestCase):
     def test_gen(self):
-        tests = [
-            ('cat', 'kat', {'KAT'}),
-            ('car', 'kar', {'KAR'}),
-            ('cart', 'kart', {'KART'}),
-
-            ('strap', 'strap', {'STRAP'}),
-
-            ('ha', 'ha', {'HA'}),
-            ('haha', 'haha', {'HA/HA'}),
-            ('hahaha', 'hahaha', {'HA/HA/HA'}),
-
-            ('Gwen', 'gwɛn', {'TKPWU/WEPB'}),
-            ('segue', 'sɛgwɛj', {'SEG/WAEU'}),
-
-            ('sled', 'slɛd', {'SHRED'}),
-            ('shred', 'ʃrɛd', {'SKHRED', 'SHU/RED'}),
-        ]
-
-        for word, pron, expected in tests:
-            result = {'/'.join(map(str, outline)) for outline in gen(pron)}
-            self.assertEqual(result, expected)
+        with open('test.txt') as f:
+            pars = f.read().split('\n\n')
+            for par in pars:
+                par = par.strip()
+                word, pron, *outlines = par.splitlines()
+                letters = word.split()
+                symbols = pron.split()
+                pairs = list(zip(letters, symbols, strict=True))
+                result = set(map(tuple, gen(pairs)))
+                expected = {
+                    tuple(map(Stroke, outline.strip().split('/')))
+                    for outline in outlines
+                }
+                self.assertEqual(result, expected)
 
 if __name__ == '__main__':
     unittest.main()
