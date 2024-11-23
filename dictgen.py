c7855216f523bdedfb26c6d15e40414c98f582d9
Nick Yeow <ytknick@gmail.com>
2024-11-23 12:35:31 +0800

    Syllabification rough sketch

--- dictgen.py
+++ dictgen.py
@@ -1,14 +1,38 @@
 import re
 from dataclasses import dataclass
+from functools import total_ordering
+from itertools import pairwise
 
 from plover_stroke import BaseStroke
 
+def divide(lst):
+    return [(lst[:i], lst[i:]) for i in range(len(lst)+1)]
+
 @dataclass
+@total_ordering
 class Sound:
     symbols: str
     stressed: bool
     length: int
     letters: str
+
+    def is_consonant(self):
+        return not(self.length)
+
+    # TODO don't just check stress?
+    def __eq__(self, other):
+        if not isinstance(other, Sound):
+            return NotImplemented
+        return self.stressed == other.stressed
+
+    def __lt__(self, other):
+        if not isinstance(other, Sound):
+            return NotImplemented
+        return not self.stressed and other.stressed
+
+    def __repr__(self):
+        stress_mark = "'" if self.stressed else ''
+        return '/' + stress_mark + self.symbols + '/'
 
 VOWEL = re.compile(r'([aɑɛɪɔoɵʉʌə])(\u0301?)([ːjw]?)')
 
@@ -26,6 +50,59 @@ def to_sounds(pairs):
             length = 0
         sounds.append(Sound(symbols, stressed, length, letters))
     return sounds
+
+def separate(sounds):
+    consonant_clusters = []
+    consonant_cluster = []
+    vowels = []
+    for sound in sounds:
+        if sound.is_consonant():
+            consonant_cluster.append(sound)
+        else: # vowel
+            consonant_clusters.append(consonant_cluster)
+            consonant_cluster = []
+            vowels.append(sound)
+    consonant_clusters.append(consonant_cluster)
+    if not vowels:
+        raise ValueError('no vowel')
+    return consonant_clusters, vowels
+
+# TODO
+def is_possible_onset(consonant_cluster):
+    return True
+
+def is_possible_coda(consonant_cluster):
+    return True
+
+def combine(nodes):
+    result = [[]]
+    for node in nodes:
+        result = [product+choice for product in result for choice in node]
+    return result
+
+def syllabify(sounds):
+    consonant_clusters, vowels = separate(sounds)
+    assert len(consonant_clusters) >= 2
+    assert len(vowels) >= 1
+    consonant_clusters = iter(consonant_clusters)
+    initial_consonant_cluster = next(consonant_clusters)
+    nodes = [[initial_consonant_cluster]]
+    right_vowel = vowels[0] # default for the shortest case
+                            # (consonant cluster, vowel, consonant cluster)
+    for (left_vowel, right_vowel), consonant_cluster in zip(pairwise(vowels),
+                                                            consonant_clusters):
+        nodes.append([[left_vowel]])
+        divisions = divide(consonant_cluster)
+        if left_vowel > right_vowel:
+            divisions.pop(0) # stronger left vowel attracts at least one consonant
+        elif left_vowel < right_vowel:
+            divisions.pop() # stronger right vowel attracts at least one consonant
+        nodes.append([[*coda, None, *onset]
+                      for coda, onset in divisions
+                      if is_possible_coda(coda) and is_possible_onset(onset)])
+    final_consonant_cluster = next(consonant_clusters)
+    nodes.extend([[[right_vowel]], [final_consonant_cluster]])
+    return combine(nodes)
 
 class Stroke(BaseStroke):
     pass
@@ -51,12 +128,6 @@ LEFT_CONSONANTS, VOWELS, RIGHT_CONSONANTS = (
     read_chords(f'chords/{basename}.txt')
     for basename in ['left', 'mid', 'right']
 )
-
-def prefixes(pairs):
-    prefix = []
-    for pair in pairs:
-        prefix.append(pair)
-        yield prefix, pairs[len(prefix):]
 
 ODD_CASES = {
     (Stroke('SH'), Stroke('R')),
