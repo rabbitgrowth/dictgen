47d0e0dc9122c141a612907f143633c07fc1d5c8
Nick Yeow <ytknick@gmail.com>
2024-11-26 18:42:54 +0800

    Alternative format for pronunciation

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 import unittest
 
 from stroke import Stroke
-from dictgen import Sound, syllabify, gen
+from dictgen import parse_pron, syllabify, gen
 
 class TestDictgen(unittest.TestCase):
-    def T(self, word, pron, outlines):
-        pairs = zip(word.split(' '), pron.split(' '), strict=True)
-        sounds = [Sound(ipa, spelling) for spelling, ipa in pairs]
+    def T(self, pron, outlines):
+        sounds = parse_pron(pron)
         result = {
             outline
             for syllables in syllabify(sounds)
@@ -19,53 +18,53 @@ class TestDictgen(unittest.TestCase):
         self.assertEqual(result, expected)
 
     def test_consonant_vowel_consonant(self):
-        self.T('c a t', 'k á t', 'KAT')
-        self.T('d o g', 'd ɔ́ g', 'TKAUG')
+        self.T('k á t', 'KAT')
+        self.T('d ɔ́ g', 'TKAUG')
 
     def test_consonants_vowel_consonants(self):
-        self.T('s c r i p t', 's k r ɪ́ p t', 'SKREUPT')
+        self.T('s k r ɪ́ p t', 'SKREUPT')
 
     def test_compound_sounds(self):
-        self.T('s l e d', 's l ɛ́ d', 'SHRED')
-        self.T('sh r e d', 'ʃ r ɛ́ d', 'SKHRED SHU/RED')
+        self.T('s l ɛ́ d', 'SHRED')
+        self.T('ʃ r ɛ́ d', 'SKHRED SHU/RED')
 
     def test_silent_h(self):
-        self.T('h eir', ' ɛ́ː', 'HAEUR')
-        self.T('air', 'ɛ́ː', 'AEUR')
+        self.T('[h] ɛ́ː', 'HAEUR')
+        self.T('ɛ́ː', 'AEUR')
 
     def test_wh_pronounced_w(self):
-        self.T('w h a t', 'w  ɔ́ t', 'WHAUT')
+        self.T('w [h] ɔ́ t', 'WHAUT')
 
     @unittest.expectedFailure
     def test_wh_pronounced_h(self):
-        self.T('w h o le', ' h ə́w l', 'HOEL')
+        self.T('[w] h ə́w l', 'HOEL')
 
     def test_ight(self):
-        self.T('r i te',  'r ɑ́j t', 'RAOEUT')
-        self.T('r igh t', 'r ɑ́j t', 'ROEUGT')
+        self.T('r ɑ́j t', 'RAOEUT')
+        self.T('r ɑ́j[igh] t', 'ROEUGT')
 
     def test_igh_t(self):
-        self.T('h igh  t ai l', 'h ɑ́j . t ɛj l', 'HAOEU/TAEUL')
+        self.T('h ɑ́j[igh] . t ɛj l', 'HAOEU/TAEUL')
 
     def test_consonants_out_of_steno_order(self):
-        self.T('G w e n', 'g w ɛ́ n', 'TKPWU/WEPB')
-        self.T('s e g u e', 's ɛ́ g w ɛj', 'SEG/WAEU')
+        self.T('g w ɛ́ n', 'TKPWU/WEPB')
+        self.T('s ɛ́ g w ɛj', 'SEG/WAEU')
 
     def test_left_vowel_attracts_consonant(self):
-        self.T('f o ll ow', 'f ɔ́ l əw', 'TPAUL/OE')
+        self.T('f ɔ́ l əw', 'TPAUL/OE')
 
     @unittest.expectedFailure
     def test_left_vowel_attracts_consonants(self):
-        self.T('i n t e r e s t', 'ɪ́ n t  r ɛ s t', 'EUPB/TR*ES EUPBT/R*ES')
+        self.T('ɪ́ n t r ɛ s t', 'EUPB/TR*ES EUPBT/R*ES')
 
     def test_right_vowel_attracts_consonant(self):
-        self.T('a b ou t', 'ə b áw t', 'U/PWOUT')
+        self.T('ə b áw t', 'U/PWOUT')
 
     def test_right_vowel_attracts_consonants(self):
-        self.T('c o m p l e te', 'k ə m p l ɪ́j t', 'KUPL/PHRAOET KUFPL/HRAOET')
+        self.T('k ə m p l ɪ́j t', 'KUPL/PHRAOET KUFPL/HRAOET')
 
     def test_not_start_with_impossible_onset(self):
-        self.T('a d m i t', 'ə d m ɪ́ t', 'UD/PHEUT') # not U/TKPHEUT
+        self.T('ə d m ɪ́ t', 'UD/PHEUT') # not U/TKPHEUT
 
 if __name__ == '__main__':
     unittest.main()
