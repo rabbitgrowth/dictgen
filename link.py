6ec3e4829f78afb3cb6cfb1a0bb82451f119c9c9
Nick Yeow <ytknick@gmail.com>
2024-12-24 15:30:44 +0800

    First attempt at linking

---
+++ link.py
@@ -0,0 +1,34 @@
+from sound import Sound
+
+PAIRS = []
+
+def link(word, pron):
+    sounds = list(map(Sound.from_ipa, pron.split()))
+    try:
+        pairs = next(pair(word, sounds))
+    except StopIteration:
+        raise ValueError(f'Failed to link "{word}" to "{pron}"')
+    for spell, sequence in pairs:
+        if not sequence:
+            yield Sound('', spelled=spell)
+        else:
+            for i, sound in enumerate(sequence):
+                if not i:
+                    sound.spelled = spell
+                    sound.cont = False
+                else:
+                    sound.cont = True
+                yield sound
+
+def pair(word, sounds, pairs=[]):
+    if not word and not sounds:
+        yield pairs
+        return
+    for spell, pattern in PAIRS:
+        sequence = sounds[:len(pattern)]
+        if word.startswith(spell) and pattern == sequence:
+            yield from pair(
+                word  [len(spell)  :],
+                sounds[len(pattern):],
+                pairs + [(spell, sequence)]
+            )
