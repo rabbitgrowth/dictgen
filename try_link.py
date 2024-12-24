6633cf985e319c12162a078fcba296f6f2900221
Nick Yeow <ytknick@gmail.com>
2024-12-24 11:38:38 +0000

    Print to terminal as well

--- try_link.py
+++ try_link.py
@@ -11,13 +11,16 @@ def length(item):
     return len(item.replace(STRESS, ''))
 
 with open('dict.tsv') as f, open('links.txt', 'w') as g:
+    def tee(string):
+        print(string, end='')
+        g.write(string)
     for i, line in enumerate(f):
         word, pron = line.strip().split('\t')
         sounds = link(word, pron)
         pairs = [(show(sound), sound.spelled) for sound in sounds]
         lengths = [max(map(length, pair)) for pair in pairs]
         if i:
-            g.write('\n')
+            tee('\n')
         for items in reversed(list(zip(*pairs))):
-            g.write(' '.join(item.ljust(length) for item, length in zip(items, lengths)).strip())
-            g.write('\n')
+            tee(' '.join(item.ljust(length) for item, length in zip(items, lengths)).strip())
+            tee('\n')
