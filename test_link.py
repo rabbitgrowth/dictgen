fcb8bd4fa5bce297239227ef7c1f5ccfea3ffe15
Nick Yeow <ytknick@gmail.com>
2025-01-04 08:22:35 +0000

    More tests

--- test_link.py
+++ test_link.py
@@ -8,18 +8,40 @@ class TestLink(unittest.TestCase):
         result = list(link(word, ipa))
         self.assertEqual(result, expected)
 
-    def test_simple_case(self):
-        self.T('a', 'ɛj', [
-            Sound('ɛj', 'a'),
+    def test_silent_letter(self):
+        self.T('back', 'b á k', [
+            Sound('b', 'b'),
+            Sound('a', 'a', stressed=True),
+            Sound('', 'c'),
+            Sound('k', 'k'),
+        ])
+
+    def test_unspelled_sound(self):
+        self.T('length', 'l ɛ́ ŋ k θ', [
+            Sound('l', 'l'),
+            Sound('ɛ', 'e', stressed=True),
+            Sound('ŋ', 'ng'),
+            Sound('k', ''),
+            Sound('θ', 'th'),
         ])
 
     def test_silent_letter_and_unspelled_sound(self):
-        self.T('able', 'ɛ́j b ə l', [
-            Sound('ɛj', 'a', stressed=True),
-            Sound('b', 'b'),
+        self.T('simple', 's ɪ́ m p ə l', [
+            Sound('s', 's'),
+            Sound('ɪ', 'i', stressed=True),
+            Sound('m', 'm'),
+            Sound('p', 'p'),
             Sound('ə', ''),
             Sound('l', 'l'),
             Sound('', 'e'),
+        ])
+
+    def test_avoid_silent_letter(self):
+        self.T('area', 'ɛ́ː r ɪj ə', [
+            Sound('ɛː', 'a', stressed=True),
+            Sound('r', 'r'),
+            Sound('ɪj', 'e'), # not <ea>
+            Sound('ə', 'a'),
         ])
 
 if __name__ == '__main__':
